3d92db13fdd331fef4cb0c2b4cd8cfd74f717710
Tracy Ogutu <tracyogutu@gmail.com>
2020-01-19 19:33:44 +0300

    add news,business and profile form

---
+++ neighbor/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from . models import Neighborhood,UserProfile,Business,News
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model=News
+        exclude=['editor']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['editor','area']
+        widgets={
+            'tags':forms.CheckboxSelectMultiple,
+        }
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model=UserProfile
+        exclude=['editor']
+        widgets={
+            'tags':forms.CheckboxSelectMultiple,
+        }
